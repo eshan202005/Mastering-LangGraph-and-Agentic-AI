166647aec1c814644f0cc89332625e3dd1885faf
Eshan Ashwani Gupta <eshangupta.prof@gmail.com>
2026-06-19 14:12:18 +0530

    phase 3 - added resume chat feature

--- chatbot_project/frontend_streamlit.py
+++ chatbot_project/frontend_streamlit.py
@@ -1,17 +1,67 @@
 import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
-#for checkpointer
-CONFIG= {'configurable':{"thread_id": '1'}}
+import uuid
+
+#*****************************utility functions***********************************
+def generate_thread_id():
+    thread_id=uuid.uuid4()
+    return thread_id
+
+def reset_chat():
+    thread_id=generate_thread_id()
+    st.session_state['thread_id']=thread_id
+    add_thread(st.session_state['thread_id'])
+    st.session_state['message_history']=[]
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+
+def load_convo(thread_id):
+    return chatbot.get_state(config =  {'configurable':{"thread_id":thread_id}}).values['messages']
+
+#*****************session setup***************************************************
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history']=[]
-#print convo history 
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id']= generate_thread_id()
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads']=[]
+add_thread(st.session_state['thread_id'])
+
+
+#*****************sidebar************************************************************
+st.sidebar.title("LnagGraph Chatbox")
+if st.sidebar.button('new chat'):
+    reset_chat()
+st.sidebar.header('my conversations')
+for thread_id in st.session_state['chat_threads']:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state['thread_id']=thread_id
+        messages = load_convo(thread_id)
+# for making the output compatible with message _ history session state
+        temp_messages = []
+        for message in messages:
+            if isinstance(message, HumanMessage):
+                role = 'user'
+            else:
+                role= 'assistant'
+        
+            temp_messages.append({'role': role , 'content':message.content})    
+        st.session_state['message_history']= temp_messages    
+        
+
+#loading convo history
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
 
 
+CONFIG = {'configurable':{"thread_id": st.session_state['thread_id']}}
 user_input = st.chat_input('Type here')    
 if user_input:
     #add to the session state
@@ -24,7 +74,7 @@ if user_input:
         ai_message = st.write_stream( 
             message_chunk.content for message_chunk, metadata in chatbot.stream(
                 {"messages": [HumanMessage(content= user_input)]},
-                config = {'configurable':{"thread_id": '1'}},
+                config = CONFIG,
                 stream_mode= "messages"
 
             )
--- chatbot_project/langgraph_backend.py
+++ chatbot_project/langgraph_backend.py
@@ -30,3 +30,4 @@ graph.add_edge("chat_node",END)
 
 
 chatbot=graph.compile(checkpointer=checkpointer)
+
